165d00f376a95eb9631ac8fc223b45b36dcfea7c
JetProc <soonjae8297@gmail.com>
2025-12-27 04:42:31 +0900

    feat: implement routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import MobileShell from "@/components/common/MobileShell";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import MobileShell from '@/components/common/MobileShell';
+import ScrollToTop from '@/components/common/ScrollToTop'; // ✅ 추가
 
-import HomeMapPage from "@/pages/home/HomeMapPage";
-import RoomDetailPage from "@/pages/home/RoomDetailPage";
-import ReservePage from "@/pages/reserve/ReservePage";
-import CommunityPage from "@/pages/community/CommunityPage";
-import MyPage from "@/pages/my/MyPage";
+import HomeMapPage from '@/pages/home/HomeMapPage';
+import RoomDetailPage from '@/pages/home/RoomDetailPage';
+import ReservePage from '@/pages/reserve/ReservePage';
+import PaymentPage from '@/pages/payment/PaymentPage';
+import CommunityPage from '@/pages/community/CommunityPage';
+import MyPage from '@/pages/my/MyPage';
 
 function App() {
-    return (
-        <MobileShell>
-            <Routes>
-                <Route path="/" element={<Navigate to="/home" replace />} />
+  return (
+    <MobileShell>
+      <ScrollToTop /> {/* ✅ 페이지 이동 시 무조건 스크롤 최상단으로 */}
+      <Routes>
+        <Route path='/' element={<Navigate to='/home' replace />} />
 
-                <Route path="/home" element={<HomeMapPage />} />
-                <Route path="/home/rooms/:roomId" element={<RoomDetailPage />} />
+        <Route path='/home' element={<HomeMapPage />} />
+        <Route path='/home/rooms/:roomId' element={<RoomDetailPage />} />
 
-                <Route path="/reserve" element={<ReservePage />} />
-                <Route path="/community" element={<CommunityPage />} />
-                <Route path="/my" element={<MyPage />} />
+        <Route path='/reserve' element={<ReservePage />} />
+        <Route path='/payment' element={<PaymentPage />} />
 
-                <Route path="*" element={<Navigate to="/home" replace />} />
-            </Routes>
-        </MobileShell>
-    );
+        <Route path='/community' element={<CommunityPage />} />
+        <Route path='/my' element={<MyPage />} />
+
+        <Route path='*' element={<Navigate to='/home' replace />} />
+      </Routes>
+    </MobileShell>
+  );
 }
 
 export default App;
